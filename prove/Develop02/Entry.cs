7722c93b3a28032f90c5fc8ac88e4f662f890a34
Prashab Gharti Chetri <pchetri8888@gmail.com>
2025-02-08 21:52:09 -0700

    Create Entry.cs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,20 @@
+public class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+    public int Rating { get; set; } // Added rating property
+
+    public Entry(string prompt, string response, string date, int rating)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+        Rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\nRating: {Rating}/5\n";
+    }
+}
