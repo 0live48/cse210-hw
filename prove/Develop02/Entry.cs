ab806f1b9f675087e24225e2b13ca89029f4f85c
Prashab Gharti Chetri <pchetri8888@gmail.com>
2025-02-08 21:53:14 -0700

    Update Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,7 +3,7 @@ public class Entry
     public string Prompt { get; set; }
     public string Response { get; set; }
     public string Date { get; set; }
-    public int Rating { get; set; } // Added rating property
+    public int Rating { get; set; } 
 
     public Entry(string prompt, string response, string date, int rating)
     {
